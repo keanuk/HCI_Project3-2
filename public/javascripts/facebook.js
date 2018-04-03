71bd27cb5dca590c198ed78f7bf5f5ab0193775c
Keanu Kerr <keanukerr@gmail.com>
2018-04-03 12:31:46 -0400

    Routing for profiles

---
+++ public/javascripts/facebook.js
@@ -0,0 +1,59 @@
+let orgs = ['alachuahumane.org', 'AARP', 'sustainableuf', 'ShandsArtsinMedicine', 'HarnMuseumofArt', 'gatorsmiles', 'gatorpalsapp2017'];
+
+window.fbAsyncInit = function() {
+FB.init({
+  appId      : '291380158062880',
+  cookie     : true,
+  xfbml      : true,
+  version    : 'v2.12'
+});
+
+// FB.AppEvents.logPageView();
+
+FB.getLoginStatus(function(response) {
+    statusChangeCallback(response);
+});
+
+};
+
+(function(d, s, id){
+ var js, fjs = d.getElementsByTagName(s)[0];
+ if (d.getElementById(id)) {return;}
+ js = d.createElement(s); js.id = id;
+ js.src = "https://connect.facebook.net/en_US/sdk.js";
+ fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+function statusChangeCallback(response) {
+ if(response.status === 'connected') {
+   console.log('Logged in and authenticated');
+   console.log("Access token: " + response.authResponse.accessToken);
+   // let at = response.authResponse.accessToken;
+   let at = 'EAACEdEose0cBAKuIQxNGRYoipoGizISxnjZAnRXqW3PZBmEoYFrZABrxffJII54zIvA0QvOPZADqhutZA4z3dGgQiQ2MVh8USdFMvB7jAhWdtUuMIUtg3nGmfHJ2VD8PFZBcwSYuOzln0UZAZAZCkTLZC4say86ZAYsVUvokRJ2PftwWMTh20rcK43NX0dmZAHqGy2ZAKZAlEYeWeIWdYgJY9JiWDh';
+
+   for(org in orgs) {
+     populateDB(orgs[org], at);
+   }
+
+ }
+ else {
+   console.log('Not authenticated');
+ }
+}
+
+function checkLoginState() {
+FB.getLoginStatus(function(response) {
+  statusChangeCallback(response);
+});
+}
+
+function populateDB(org, at) {
+FB.api('/' + org + '?fields=name,events' + '&access_token=' + at, function(response) {
+  if(response && !response.error) {
+    console.log(response);
+  }
+  else {
+    console.log(response.error);
+  }
+})
+}
